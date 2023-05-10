a6f0626398d1bad009f475baeb0e82503089c3b1
JP-GZ <60190108+JP-GZ@users.noreply.github.com>
2023-05-09 23:46:03 -0600

    Primer Commit

--- data.py
+++ data.py
@@ -1,7 +1,6 @@
-
 """
 # -- --------------------------------------------------------------------------------------------------- -- #
-# -- project: A SHORT DESCRIPTION OF THE PROJECT                                                         -- #
+# -- PROYECTO: ANALISIS TÉCNICO                                                        -- #
 # -- script: data.py : python script for data collection                                                 -- #
 # -- author: YOUR GITHUB USER NAME                                                                       -- #
 # -- license: THE LICENSE TYPE AS STATED IN THE REPOSITORY                                               -- #
@@ -9,4 +8,43 @@
 # -- --------------------------------------------------------------------------------------------------- -- #
 """
 
-dict_test = {'key_a': 'a', 'key_b': 'b'}
+from datetime import datetime
+
+import pandas as pd
+import pytz
+import numpy as np
+import functions as fun
+import pyswarms as ps
+from pyswarms.utils.functions import single_obj as fx
+
+data_Test=pd.read_csv('files/BTC2021_2.csv')
+data_Train=pd.read_csv('files/BTC2022.csv')
+
+data_Test = fun.technicals(data_Test).dropna()
+#data_Test = fun.tec2(data_Test).dropna()
+# data_Train = fun.technicals(data_Train).dropna()
+
+
+vol=0.5 # volumen en bitcoin
+pips=9000 # exposicion por operacion 100 usd
+df = data_Test[(data_Test['stochastic_buy_signal']==True) | (data_Test['stochastic_sell_signal']==True)] # df con las señales de compra y venta
+
+precio_transaccion = df['close'].iloc[0]
+posicion = precio_transaccion * vol
+day0 = (df.iloc[0, 0])
+print(df['close'].iloc[0])
+print(df.iloc[0, 0])
+
+closed = df[(df['close'] >= (precio_transaccion + pips / 100)) | (df['close'] <= (precio_transaccion - pips / 100))]
+print(closed)
+closed_price = closed.iloc[0, 5]
+print(closed_price)
+closed_transaction = closed_price * vol
+print(closed_transaction)
+day1 = (closed.iloc[0, 0])
+print(day1)
+print((day1 - day0))
+ret = 246 * (closed_transaction / posicion - 1)/ (day1 - day0)
+# transacciones = fun.transaccion(vol, pips, df)
+
+# print(transacciones)
--- functions.py
+++ functions.py
@@ -8,3 +8,84 @@
 # -- repository: YOUR REPOSITORY URL                                                                     -- #
 # -- --------------------------------------------------------------------------------------------------- -- #
 """
+
+# import MetaTrader5 as mt5
+import pytz
+from datetime import datetime
+import pandas as pd
+from ta.volatility import BollingerBands
+from ta.momentum import StochasticOscillator
+from ta.trend import MACD
+
+
+# def import_data():
+#     # mt5.initialize()
+#     if not mt5.initialize():
+#         print("initialize() failed, error code =", mt5.last_error())
+#         quit()
+#     # set time zone to UTC
+#     timezone = pytz.timezone("Etc/UTC")
+#     # create 'datetime' objects in UTC time zone to avoid the implementation of a local time zone offset
+#     utc_from = datetime(2020, 2, 1, tzinfo=timezone)
+#     utc_to = datetime(2021, 2, 1, tzinfo=timezone)
+#     rates = mt5.copy_rates_range("BITCOIN", mt5.TIMEFRAME_D1, utc_from, utc_to)
+#     # create DataFrame out of the obtained data
+#     rates_frame = pd.DataFrame(rates)
+#     # rates_frame['time'] = pd.to_datetime(rates_frame['time'], unit='s')
+#     rates_frame.to_csv('BTC2021_2.csv')
+#     return rates_frame
+
+def technicals(data):
+    data = data.copy()
+    bollinger = BollingerBands(close=data.close, window=21)
+    stoc = StochasticOscillator(high=data['high'], low=data['low'], close=data['close'])
+    macd = MACD(close=data['close'])
+    # ------------------------------------------------------------------------------------
+    data['bb_bbm'] = bollinger.bollinger_mavg()
+    data['bb_bbh'] = bollinger.bollinger_hband()
+    data['bb_bbl'] = bollinger.bollinger_lband()
+    data['bb_high_signal'] = bollinger.bollinger_hband_indicator()
+    data['bb_low_signal'] = bollinger.bollinger_lband_indicator()
+    # ------------------------------------------------------------------------------------
+    data['stochastic'] = stoc.stoch()
+    data['stochastic_buy_signal'] = stoc.stoch() < 20
+    data['stochastic_sell_signal'] = stoc.stoch() > 80
+    # ------------------------------------------------------------------------------------
+    data['macd'] = macd.macd()
+    data['macd_signal'] = macd.macd_signal()
+    data['macd_hist'] = macd.macd_diff()
+    data['macd_buy_signal'] = macd.macd() > macd.macd_signal()
+    data['macd_sell_hist'] = macd.macd() < macd.macd_diff()
+
+    data = data.drop(['Unnamed: 0'], axis=1)
+    return data
+def tec2(data):
+    df = data.copy()
+    indicator_stoc = StochasticOscillator(high=data['high'], low=data['low'], close=data['close'])
+    df['stochastic'] = indicator_stoc.stoch()
+    df['stochastic_buy_signal'] = indicator_stoc.stoch() < 20
+    df['stochastic_sell_signal'] = indicator_stoc.stoch() > 80
+    return df
+
+
+def transaccion(vol, pips, df):
+    rett = []
+
+    while len(df) > 2:
+        precio_transaccion = df['close'].iloc[0]
+        posicion = precio_transaccion * vol
+        day0 = df.iloc[0, 0]
+
+        closed = df[
+            (df['close'] >= (precio_transaccion + pips / 100)) | (df['close'] <= (precio_transaccion - pips / 100))]
+        closed_price = closed.iloc[0, 5]
+        closed_transaction = closed_price * vol
+        day1 = closed.iloc[0, 0]
+
+        ret = 246 * (closed_transaction / posicion - 1) / (day1 - day0)
+
+        rett.append(ret)
+
+        df = df.loc[closed.iloc[0, 0]:]
+
+    return rett
