ec143b2132168100ce6618eee4393b4ecd3e76b5
JP-GZ <60190108+JP-GZ@users.noreply.github.com>
2023-05-11 23:28:53 -0600

    Final de funciones

--- data.py
+++ data.py
@@ -17,15 +17,44 @@ import functions as fun
 import pyswarms as ps
 from pyswarms.utils.functions import single_obj as fx
 
-data_Test=pd.read_csv('files/EURUSD2021.csv')
-data_Train=pd.read_csv('files/EURUSD2022.csv')
+def extraccion_data():
+    data_Test = pd.read_csv('files/EURUSD2021.csv')
+    data_Train = pd.read_csv('files/EURUSD2022.csv')
+    return data_Test,data_Train
+
+def entrenamiento(df:pd.DataFrame) -> pd.DataFrame:
+    data = df.query('stochastic_buy_signal == True or stochastic_sell_signal == True')
+    return data
+
+data_Test,data_Train = extraccion_data()
 
 data_Test = fun.technicals(data_Test).dropna()
 data_Test['time'] = pd.to_datetime(data_Test['time'], format='%Y-%m-%d')
-capital_inicial = 100_000
-vol=0.5
-pips=1
+
+data_Train = fun.technicals(data_Train).dropna()
+data_Train['time'] = pd.to_datetime(data_Train['time'], format='%Y-%m-%d')
+# -------------------------------------------------------------------------
 df = data_Test.query('stochastic_buy_signal == True or stochastic_sell_signal == True')
-print("---------------------------------")
-print(df.columns)
+traiding_test = fun.tradear(df,0.03, 0.03, 8000)
+print(f'Profit = {traiding_test:.4f}')
+
+# opt_vals = fun.busqueda_exhaustiva(data_Train)
+# trading_test = fun.tradear(data_Test, opt_vals["stop_loss"], opt_vals["take_profit"], opt_vals["volumen"])
+# Se obtuvo como resultado
+
+"""
+stop_loss          0.026789
+take_profit        0.001000
+volumen         5000.000000
+profit         99716.664875
+"""
+
+# trading_ex_test = fun.tradear(data_Test, 0.026789, 0.001000, 5000)
+# print(f"Profit busqueda exhaustiva: ${trading_ex_test:,.4f}")
+
+# best_cost, best_args = fun.busqueda_pso(data_Train)
+# Optimizando por pso se obtuvo
+# (97086.54226331878, array([9.88641118e-03, 3.20993169e-02, 1.46582293e+04]))
+# trading_pso_test = fun.tradear(data_Test, 9.88641118e-03, 3.20993169e-02, 1.46582293e+04)
+# print(f"Profit busqueda pso: ${trading_pso_test:,.4f}")
 
--- functions.py
+++ functions.py
@@ -13,9 +13,11 @@ import MetaTrader5 as mt5
 import pytz
 from datetime import datetime
 import pandas as pd
+import tqdm
 from ta.volatility import BollingerBands
 from ta.momentum import StochasticOscillator
 from ta.trend import MACD
+import numpy as np
 
 
 def import_data():
@@ -59,22 +61,117 @@ def technicals(data):
     data = data.drop(['Unnamed: 0'], axis=1)
     return data
 
-def transaccion(vol, pips, df):
-    rett = []
+def tradear(data: pd.DataFrame, sl: float, tp: float, vol: int):
+    operaciones_cerradas = []
+    operaciones_activas = []
+    # Hacemos hasta 10 operaciones
+    max_num_operaciones = 10
+    cash = 100_000
+    valor_port = [cash]
 
-    while len(df) > 2:
-        precio_transaccion = df['close'].iloc[0]
-        posicion = precio_transaccion * vol
+    for _, price_stats in data.iterrows():
+        precio = price_stats["close"]
+        fecha = price_stats["time"]
+        buy_signals = [price_stats["bb_low_signal"], price_stats["stochastic_buy_signal"],
+                       price_stats["macd_buy_signal"]]
+        sell_signals = [price_stats["bb_high_signal"], price_stats["stochastic_sell_signal"],
+                        price_stats["macd_sell_hist"]]
 
-        cierres = df.query('(close >= @precio_transaccion + @pips/100) or (close <= @precio_transaccion - @pips/100)')
-        precio_cierre = cierres.iloc[0, 5]
-        closed_transaction = precio_cierre * vol
-        day0 = df.iloc[0, 0]
-        day1 = cierres.iloc[0, 0]
-        days = (day1 - day0)
-        days = int(days.total_seconds() + days.days * pd.Timedelta(days=1).total_seconds())
-        ret = 246 * (closed_transaction / posicion - 1) / days
-        rett.append(ret)
-        df = df.loc[cierres.iloc[0, 0]:]
+        if sum(buy_signals) >= 2 and len(operaciones_activas) < max_num_operaciones and cash > (
+                (10_000 // precio) * 1.00125):
+            # Sacar numero de titulos
+            num_titulos = vol
+            # Sacar costo de compra
+            costo = num_titulos * precio
+            # Agregar comisiones
+            costo_com = costo * 1.00125
+            # Descontar al cash
+            cash -= costo_com
+            # Poner stop loss y take profit
+            stop_loss = precio * (1 - sl)
+            take_profit = precio * (1 + tp)
+            # agregar a operaciones activas
+            operaciones_activas.append({
+                "fecha": fecha,
+                "precio_compra": precio,
+                "num_titulos": num_titulos,
+                "costo": costo,
+                "cost_com": costo_com,
+                "stop_loss": stop_loss,
+                "take_profit": take_profit
+            })
+            # print(f"Comprando {num_titulos} a {precio} - Cash {cash}")
 
-    return rett
+        # Checar sl y tp
+        for operacion_activa in operaciones_activas:
+
+            if precio < operacion_activa["stop_loss"] or precio > operacion_activa["take_profit"]:
+                tipo = "stop loss" if precio < operacion_activa["stop_loss"] else "take profit"
+                operacion_activa["fecha_cierre"] = fecha
+                operacion_activa["precio_venta"] = precio
+                operacion_activa["valor_port"] = cash + operacion_activa["num_titulos"] * precio
+                num_titulos = operacion_activa["num_titulos"]
+                venta = num_titulos * precio
+                venta_com = venta * (1 - 0.00125)  # Comision descontada
+                cash += venta_com
+                operaciones_cerradas.append(operacion_activa)
+                operaciones_activas.remove(operacion_activa)
+                #print(f"Vendiendo por {tipo} {num_titulos} a {precio} - Cash {cash}")
+
+        if sum(sell_signals) >= 2:
+            for operacion_activa in operaciones_activas:
+                operacion_activa["fecha_cierre"] = fecha
+                operacion_activa["precio_venta"] = precio
+                operacion_activa["valor_port"] = cash + operacion_activa["num_titulos"] * precio
+                num_titulos = operacion_activa["num_titulos"]
+                venta = num_titulos * precio
+                venta_com = venta * (1 - 0.00125)  # Comision descontada
+                cash += venta_com
+                operaciones_cerradas.append(operacion_activa)
+                # print(f"Vendiendo {num_titulos} a {precio} - Cash {cash}")
+            operaciones_activas = []
+
+        # Historico del valor del portafolio
+        if operaciones_activas:
+            num_titulos = sum(list(map(lambda operacion: operacion["num_titulos"], operaciones_activas)))
+            valor_port.append(cash + num_titulos * precio)
+        else:
+            valor_port.append(cash)
+
+    return valor_port[-1]
+
+
+def busqueda_exhaustiva(data):
+    sl_ = np.linspace(0.001, 0.05, 20)
+    tp_ = np.linspace(0.001, 0.05, 20)
+    vol_ = np.linspace(5000, 15000, 50)
+    resultados = {
+        "stop_loss": [],
+        "take_profit": [],
+        "volumen": [],
+        "profit": []
+    }
+    for i in tqdm.tqdm(range(len(sl_))):
+        sl = sl_[i]
+        for tp in tp_:
+            for vol in vol_:
+                resultados["stop_loss"].append(sl)
+                resultados["take_profit"].append(tp)
+                resultados["volumen"].append(vol)
+                resultados["profit"].append(tradear(data, sl, tp, vol))
+
+    res_df = pd.DataFrame(resultados)
+    profits = res_df.loc[:, "profit"].values
+    max_profit_idx = np.argmax(profits)
+    return res_df.iloc[max_profit_idx, :]
+
+
+def busqueda_pso(data):
+    rango_min = np.array([0.001, 0.001, 5000])  # sl, tp, vol
+    rango_max = np.array([0.05, 0.05, 15000])  # sl, tp, vol
+    bounds = (rango_min, rango_max)
+
+    options = {'c1': 0.5, 'c2': 0.3, 'w': 0.9}
+    optimizer = ps.single.GlobalBestPSO(n_particles=10, dimensions=3, options=options, bounds=bounds)
+    best_cost, best_pos = optimizer.optimize(lambda args: np.array([tradear(data, arg[0], arg[1], arg[2]) for arg in args]), iters=100)
+    return best_cost, best_pos
